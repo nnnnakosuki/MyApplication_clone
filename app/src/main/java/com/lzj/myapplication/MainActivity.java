9df34767efb34ec9dea84c7bc892d6649310fcdd
lzj <2452611476@qq.com>
2024-03-27 20:22:40 +0800

    Initial commit

---
+++ app/src/main/java/com/lzj/myapplication/MainActivity.java
@@ -0,0 +1,51 @@
+package com.lzj.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.nfc.Tag;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    int ans = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("aaaaaaa","onCreate: ");
+        setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v){
+        setContentView(R.layout.activity_main);
+        TextView out1 = findViewById(R.id.score);
+//        int ans1 = Integer.parseInt(out1.getText().toString());
+        ans += 1;
+        out1.setText(String.valueOf(ans));
+    }
+    public void click2(View v){
+        setContentView(R.layout.activity_main);
+        TextView out2 = findViewById(R.id.score);
+//        int ans2 = Integer.parseInt(out2.getText().toString());
+        ans += 2;
+        out2.setText(String.valueOf(ans));
+    }
+    public void click3(View v){
+        setContentView(R.layout.activity_main);
+        TextView out3 = findViewById(R.id.score);
+//        int ans = Integer.parseInt(out3.getText().toString());
+        ans += 3;
+        out3.setText(String.valueOf(ans));
+    }
+    public void click4(View v){
+        setContentView(R.layout.activity_main);
+        TextView out = findViewById(R.id.score);
+        ans = 0;
+        out.setText(String.valueOf(ans
+        ));
+    }
+}
